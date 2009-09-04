3c1bd67f335d75b649eb00936d16884fd3356c87
Eddie Ringle <eddie@eringle.net>
2009-09-04 08:02:33 -0400

    Initial commit.
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

---
+++ crawl.rb
@@ -0,0 +1,45 @@
+require "iconv"
+require "hpricot"
+require "open-uri"
+
+class Spider
+    def initialize(url)
+        @keywords = Hash.new(0)
+        @url = url
+        @common = [
+        'the','be','to','of','and','a','in','that','have','i','it','for','not',
+        'on','with','as','you','do','at','this','but','by','from','they',
+        'say','or','an','will','all','would','there','their','what','so','up',
+        'out','if','get','which','when','can','like','no','just','take','into',
+        'some','could','them','other','than','then','now','only','come','its',
+        'also','over','back','after','use','our','well','way','even','want',
+        'because','these','give','us','most','|','\\','/','is'
+        ]
+    end
+    def crawl()
+        f = open(@url)
+        f.rewind
+        doc = Hpricot(Iconv.conv('utf-8',f.charset,f.readlines.join("\n")))
+        content = doc.search('body')
+        if content.empty?
+            content = doc.search('BODY')
+            if content.empty?
+                throw "Invalid HTML"
+            end
+        end
+        (content/"script").remove
+        (content/"SCRIPT").remove
+        (content/"style").remove
+        (content/"STYLE").remove
+        content.inner_text.split(' ').each do |v|
+            @keywords[v] += 1 if @common.rindex(v.downcase) == nil
+        end
+        @keywords = @keywords.sort {|k,v| -1*(k[1] <=> v[1])}
+        @keywords.each do |k,v|
+            puts "#{k} appears #{v} times."
+        end
+    end
+end
+
+s = Spider.new(ARGV[0])
+s.crawl
